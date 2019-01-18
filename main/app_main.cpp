c2da16da7dc4f1b9857a54386f82eb590d10a9f2
lewisxhe <lewishe@outlook.com>
2019-01-18 20:25:02 +0800

    add display

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -33,12 +33,16 @@
 #include "app_sd.h"
 #include "app_sensor.h"
 
+/* lvgl includes */
+#include "iot_lvgl.h"
+
 static const char *TAG = "[main]";
 
 
 //Reference the binary-included jpeg file
 extern const uint8_t image_jpg_start[]   asm("_binary_plus_jpg_start");
 extern const uint8_t image_jpg_end[]     asm("_binary_plus_jpg_end");
+#define IMAGES_SIZE 28088
 
 en_fsm_state g_state = WAIT_FOR_WAKEUP;
 int g_is_enrolling = 0;
@@ -307,28 +311,140 @@ void show_data()
         if (rslt != BME280_OK) {
             return;
         }
-
         if (g_state != WAIT_FOR_WAKEUP) {
             app_sensor_deinit(&dev);
             return;
         }
-
-
         tft->fillScreen(0);
 
-
+#ifdef BME280_FLOAT_ENABLE
         snprintf(buff, sizeof(buff), "Temp    :%0.2f", comp_data.temperature);
         tft->drawString(buff, 0, 30);
         snprintf(buff, sizeof(buff), "Pressure:%0.2f", comp_data.pressure);
         tft->drawString(buff, 0, 60);
         snprintf(buff, sizeof(buff), "Humidity:%0.2f", comp_data.humidity);
         tft->drawString(buff, 0, 90);
-
-
-
+#endif
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
+
+
+static lv_obj_t *chart = NULL;
+static lv_obj_t *gauge = NULL;
+static lv_chart_series_t *series = NULL;
+static lv_chart_series_t *series1 = NULL;
+static lv_obj_t *tabview = NULL;
+static lv_obj_t *label = NULL;
+static lv_obj_t *label1 = NULL;
+static lv_obj_t *label2 = NULL;
+
+static void littlevgl_demo(void)
+{
+    lv_obj_t *scr = lv_obj_create(NULL, NULL);
+    lv_scr_load(scr);
+
+    // lv_theme_t *th = lv_theme_zen_init(100, NULL);
+    lv_theme_t *th = lv_theme_material_init(100, NULL);
+    // lv_theme_t *th = lv_theme_night_init(100, NULL);
+    // lv_theme_t *th = lv_theme_alien_init(100, NULL);
+    lv_theme_set_current(th);
+
+    tabview = lv_tabview_create(lv_scr_act(), NULL);
+
+    lv_obj_t *tab1 = lv_tabview_add_tab(tabview, "BME280");
+    // lv_obj_t *tab2 = lv_tabview_add_tab(tabview, SYMBOL_HOME);
+    // lv_obj_t *tab3 = lv_tabview_add_tab(tabview, SYMBOL_SETTINGS);
+
+    lv_tabview_set_tab_act(tabview, 0, false);
+
+    //曲线
+    chart = lv_chart_create(tab1, NULL);
+    lv_obj_set_size(chart, 240, 110);
+    lv_chart_set_point_count(chart, 20);
+    lv_obj_align(chart, NULL, LV_ALIGN_CENTER, 0, 0);
+    // lv_chart_set_type(chart, (lv_chart_type_t)(LV_CHART_TYPE_POINT | LV_CHART_TYPE_LINE));
+    lv_chart_set_type(chart, (lv_chart_type_t)LV_CHART_TYPE_COLUMN);
+
+    lv_chart_set_series_opa(chart, LV_OPA_70);
+    lv_chart_set_series_width(chart, 4);
+    lv_chart_set_range(chart, 0, 100);
+
+    series = lv_chart_add_series(chart, LV_COLOR_RED);
+    series1 = lv_chart_add_series(chart, LV_COLOR_BLUE);
+
+    //仪表盘
+    // static lv_color_t needle_colors[] = {LV_COLOR_RED, LV_COLOR_BLUE, LV_COLOR_GREEN};
+    // gauge = lv_gauge_create(tab1, NULL);
+    // lv_gauge_set_needle_count(gauge,
+    //                           sizeof(needle_colors) / sizeof(needle_colors[0]), needle_colors);
+    // lv_obj_align(gauge, NULL, LV_ALIGN_CENTER, 0, 0);
+    // lv_gauge_set_value(gauge, 0, 50);
+    // lv_gauge_set_value(gauge, 1, 60);
+    // lv_gauge_set_value(gauge, 2, 60);
+
+    label = lv_label_create(tabview, NULL);
+    lv_label_set_text(label, "1Hello Arduino!");
+    lv_obj_align(label, NULL, LV_ALIGN_OUT_BOTTOM_MID, 0,  -20);
+
+    label1 = lv_label_create(tabview, NULL);
+    lv_label_set_text(label1, "2Hello Arduino!");
+    lv_obj_align(label1, NULL, LV_ALIGN_OUT_BOTTOM_MID, 0, -40);
+
+    label2 = lv_label_create(tabview, NULL);
+    lv_label_set_text(label2, "3Hello Arduino!");
+    lv_obj_align(label2, NULL, LV_ALIGN_OUT_BOTTOM_MID, 0, -60);
+
+    // for (int i = 0; i < 4; i++) {
+    //     bme280lable[i] =  lv_label_create(tabview, NULL);
+    //     lv_label_set_text(bme280lable[i], "Hello Arduino!");
+    //     lv_obj_align(label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, i + 10);
+    // }
+}
+
+
+static void user_task(void *pvParameter)
+{
+    uint8_t value = 0;
+    struct bme280_data comp_data;
+    char buff[128];
+    while (1) {
+        int8_t rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, &dev);
+
+        if (rslt != BME280_OK) {
+            vTaskDelay(500 / portTICK_PERIOD_MS);
+            ESP_LOGE(TAG, "BME280 read error");
+            continue;
+        }
+        if (g_state != WAIT_FOR_WAKEUP) {
+            app_sensor_deinit(&dev);
+            vTaskDelete(NULL);
+            return;
+        }
+
+        lv_chart_set_next(chart, series, comp_data.temperature);
+        lv_chart_set_next(chart, series1, comp_data.humidity );
+        // ESP_LOGI(TAG, "temperature:%0.2f pressure:%0.2f humidity:%0.2f",
+        //          comp_data.temperature, comp_data.pressure, comp_data.humidity );
+
+        snprintf(buff, sizeof(buff), "press:%0.2fhP", comp_data.pressure);
+        lv_label_set_text(label, buff);
+
+        snprintf(buff, sizeof(buff), "hum  :%0.2f%%", comp_data.humidity);
+        lv_label_set_text(label1, buff);
+
+        snprintf(buff, sizeof(buff), "temp :%0.2f*C", comp_data.temperature);
+        lv_label_set_text(label2, buff);
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        // static int i = 0;
+        // i = i + 1 > 3 ? 0 : i + 1;
+        // lv_tabview_set_tab_act(tabview, i, false);
+    }
+}
+
+
 
 extern "C"  void app_main()
 {
@@ -339,10 +455,33 @@ extern "C"  void app_main()
 
     isInitBus = app_sd_init(&card);
 
+#if 1
+    app_camera_init();
+
+    lvgl_init();
+
+    littlevgl_demo();
+
+    app_speech_wakeup_init();
+
+    g_state = WAIT_FOR_WAKEUP;
+
+    app_sensor_init(&dev);
+
+    xTaskCreate(
+        user_task,   //Task Function
+        "user_task", //Task Name
+        4096,        //Stack Depth
+        NULL,        //Parameters
+        1,           //Priority
+        NULL);       //Task Handler
+
+    while (1);
+
+#else
     app_lcd_init(!isInitBus);
 
-    int size = 28088;
-    TFT_jpg_image(CENTER, CENTER, 0, -1, NULL, (uint8_t*)image_jpg_start, size);
+    TFT_jpg_image(CENTER, CENTER, 0, -1, NULL, (uint8_t *)image_jpg_start, IMAGES_SIZE);
 
     vTaskDelay(6000 / portTICK_PERIOD_MS);
 
@@ -366,7 +505,7 @@ extern "C"  void app_main()
 
     show_data();    //BME280
 
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 
     tft->drawString("Please say 'Hi LeXin' to the board", 0, 30);
 
@@ -376,5 +515,5 @@ extern "C"  void app_main()
     app_wifi_init();
 
     xTaskCreatePinnedToCore(app_lcd_task, "app_lcd_task", 4096, NULL, 4, NULL, 0);
-
+#endif
 }
